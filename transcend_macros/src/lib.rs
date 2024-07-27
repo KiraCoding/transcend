f96ccafb4e350ea03569af332333a2ef48c1737a
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-07-27 23:25:54 +0200

    wip

--- transcend_macros/src/lib.rs
+++ transcend_macros/src/lib.rs
@@ -5,13 +5,13 @@ use quote::quote;
 pub fn sig(input: TokenStream) -> TokenStream {
     let input = input.to_string().replace("??", "FF");
 
-    let byte_vector: Vec<u8> = input
+    let bytes: Vec<u8> = input
         .split_ascii_whitespace()
         .map(|hex| u8::from_str_radix(hex, 16).unwrap())
         .collect();
 
     quote! {
-        &[#(#byte_vector),*]
+        &[#(#bytes),*]
     }
     .into()
 }
